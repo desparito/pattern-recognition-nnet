3c01423272bf8204964a77b9a9b4bdecc82a69f8
amber <amber.norder@gmail.com>
2020-01-10 12:35:22 +0100

    Auto stash before merge of "master" and "origin/master"

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
+#https://www.kaggle.com/joshmiller656/classifying-movies-from-raw-image-using-convnets inspiration from this 
+#https://github.com/benckx/dnn-movie-posters/blob/master/ interesting repo
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np # pip install numpy
+import pandas as pd # pip install pandas
 
 from subprocess import check_output
 
 #Needs to be changes to the correct path when the posters are there
-path = 'Data/Posters'
-import glob
-import scipy.misc
-import imageio
-from PIL import Image
+path = 'Data/SampleMoviePosters/SampleMoviePosters'
+import glob #pip install glob
+import scipy.misc #pip install ..
+import imageio #pip install imageio
+from PIL import Image #pip install Pillow
 
 image_glob = glob.glob(path+"/"+"*.jpg")
 img_dict = {}
@@ -17,8 +19,8 @@ def get_id(filename):
     index_s = max(filename.rfind("\\")+1, filename.rfind("/")+1)
     index_f = filename.rfind(".jpg")
     return filename[index_s:index_f]
-#Populate image dict
-_ = [img_dict.update({get_id(fn):imageio.imread(fn, pilmode="RGB", as_gray=False)}) for fn in image_glob]
+#Populate image dicts
+_ = [img_dict.update({get_id(fn):imageio.imread(fn)}) for fn in image_glob]
 
 #Reads the movie genres
 df = pd.read_csv("Data/MovieGenre.csv",encoding="ISO-8859-1")
@@ -32,6 +34,7 @@ classes = list(set(genres))
 classes.sort()
 num_classes = len(classes)
 
+#TODO CHANGE THIS TO A FUNCTION WHICH RETURNS A VECTOR OF LENTH 24 WITH 0/1 FOR GENRE IT HAS GENRE IS NOW A STRING
 def get_classes_from_movie(movie_id):
     y = np.zeros(num_classes)
     g = str(df[df['imdbId']==movie_id]['Genre'].values[0])
@@ -64,13 +67,12 @@ def get_dataset(train_size,img_size=32):
                 y.append(get_classes_from_movie(id_key))
             else:
                 if not preprocess(img_dict[list(img_dict.keys())[i]],size=img_size).shape == (128,128,3):
-                    print(list(img_dict.keys())[i]) 
                 x_test.append(preprocess(img_dict[list(img_dict.keys())[i]],size=img_size))
                 y_test.append(get_classes_from_movie(id_key))
         return x,y,x_test,y_test
         
 #Constant to keep track of our image size
-SIZE = 128   
+SIZE = 128
 x,y,x_test,y_test = get_dataset(30,img_size=SIZE)
 x = np.asarray(x)
 y = np.asarray(y)
@@ -111,3 +113,12 @@ model.fit(x, y,
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+pred = model.predict(np.asarray([x_test[5]]))
+
+print("pred")
+print(pred) #predictions of all classes
+print("np.argmax pred")
+print(np.argmax(pred)) #Max class predicted
+print("y_test[5]:")
+print(np.argmax(y_test[5])) #The real class
