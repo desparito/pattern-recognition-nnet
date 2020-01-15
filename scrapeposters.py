c7997a33fc5813e0ce3a0eb9fa82aebfd7c79ca3
Brian Janssen <brian-janssen@hotmail.com>
2020-01-15 16:39:43 +0100

    Write undownloadable posters to a file

--- scrapeposters.py
+++ scrapeposters.py
@@ -7,10 +7,15 @@ df = pd.read_csv("Data/cleaned.csv", usecols=["imdbId", "Poster"])
 if not os.path.isdir("Data/Posters"):
     os.makedirs("Data/Posters")
 
+open("Data/downloaderror.csv", 'w').close()
+
+
 for row in df.itertuples(index=False):
     name = "Data/Posters/" + str(row.imdbId) + ".jpg"
     if not os.path.isfile(name):
         try:
             urllib.request.urlretrieve(row.Poster, name)
         except urllib.error.HTTPError:
-            print("Could not download id: %i"%row.imdbId)
+            errors = open("Data/downloaderror.csv", 'a')
+            errors.write(str(row.imdbId)+"\n")
+            errors.close()
