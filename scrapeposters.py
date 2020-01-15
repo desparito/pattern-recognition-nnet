d7032d38fa6efa7eedc9040a77c6188a1346f8a1
Brian Janssen <brian-janssen@hotmail.com>
2020-01-15 12:02:32 +0100

    Update scrape script by using cleaned data

--- scrapeposters.py
+++ scrapeposters.py
@@ -2,11 +2,16 @@ import pandas as pd
 import urllib.request
 import os.path
 
-df = pd.read_csv("Data/MovieGenre.csv", encoding="latin-1")
-for index, row in df.tail(4000).iterrows():
-    try:
-        if not (str(row[5]) == "nan" or os.path.isfile("Data/Posters/" + str(row[0]) + ".jpg")):
-            urllib.request.urlretrieve(row[5], "Data/Posters/" + str(row[0]) + ".jpg")
-    except urllib.error.HTTPError:
-        pass #print(row[0])
+df = pd.read_csv("Data/cleaned.csv", usecols=["imdbId", "Poster"])
 
+if not os.path.isdir("Data/Posters"):
+    os.makedirs("Data/Posters")
+
+for row in df.itertuples(index=False):
+    print("Downloading: %i"%row.imdbId)
+    name = "Data/Posters/" + str(row.imdbId) + ".jpg"
+    if not os.path.isfile(name):
+        try:
+            urllib.request.urlretrieve(row.Poster, name)
+        except urllib.error.HTTPError:
+            print("Could not download id: %i"%row.imdbId)
