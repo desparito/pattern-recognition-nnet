cf0663ece8ae057375d418f124167ebd1f629c7b
Brian Janssen <brian-janssen@hotmail.com>
2020-01-17 16:03:37 +0100

    Change output order and do asarray

--- test.py
+++ test.py
@@ -77,7 +77,12 @@ def get_dataset(train_size,img_size=(32,32)):
         else:
             x_img_test.append(preprocess(img_dict[id_key],size=img_size))
             y_test.append(classes.loc[id_key])
-    return x_img,y,x_img_test,y_test
+    return (
+        np.asarray(x_img),
+        np.asarray(x_img_test),
+        np.asarray(y),
+        np.asarray(y_test)
+        )
 
 def get_dataset_yolo(train_size, img_size=(32,32)):
     images = list(img_dict)
@@ -97,23 +102,21 @@ def get_dataset_yolo(train_size, img_size=(32,32)):
             x_img_test.append(preprocess(img_dict[id_key],size=img_size))
             x_yolo_test.append([yolo_df.loc[id_key]])
             y_test.append(classes.loc[id_key])
-    return x_img,x_yolo,y,x_img_test,x_yolo_test,y_test
+    return (
+        np.asarray(x_img),
+        np.asarray(x_img_test),
+        np.asarray(y),
+        np.asarray(y_test),
+        np.asarray(x_yolo),
+        np.asarray(x_yolo_test)
+        )
 
 #Constant to keep track of our image size
 SIZE = (128, 128)
 if USE_YOLO:
-    x_img,x_yolo,y,x_img_test,x_yolo_test,y_test = get_dataset_yolo(300,img_size=SIZE)
-    x_img = np.asarray(x_img)
-    x_yolo = np.asarray(x_yolo)
-    x_img_test = np.asarray(x_img_test)
-    x_yolo_test = np.asarray(x_yolo_test)
+    x_img, x_img_test, y, y_test, x_yolo, x_yolo_test = get_dataset_yolo(300,img_size=SIZE)
 else:
-    x_img,y,x_img_test,y_test = get_dataset(300,img_size=SIZE)
-    x_img = np.asarray(x_img)
-    x_img_test = np.asarray(x_img_test)
-
-y = np.asarray(y)
-y_test = np.asarray(y_test)
+    x_img, x_img_test, y, y_test = get_dataset(300,img_size=SIZE)
 
 print("Using model")
 
