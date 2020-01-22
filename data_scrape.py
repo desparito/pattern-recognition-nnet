39d4aa7287c11bc527f38f3175404ffd2b5d5461
Brian Janssen <brian-janssen@hotmail.com>
2020-01-22 13:07:55 +0100

    Add scraping comments

--- data_scrape.py
+++ data_scrape.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import urllib.request
 import os.path
-from joblib import Parallel, delayed
-import multiprocessing
-     
+
+# Read the to-scrape posters
 df = pd.read_csv("Data/cleaned.csv", usecols=["imdbId", "Poster"])
 
+# Create a poster directory if it does not exist yet
 if not os.path.isdir("Data/Posters"):
     os.makedirs("Data/Posters")
 
+# Define a function for downloading a single poster, ignoring it if it cannot be downloaded
 def downloadPoster(row):
     name = "Data/Posters/" + str(row.imdbId) + ".jpg"
     if not os.path.isfile(name):
@@ -17,5 +18,8 @@ def downloadPoster(row):
         except urllib.error.HTTPError:
             pass
  
+# Run the download function in parallel for each poster.
+from joblib import Parallel, delayed
+import multiprocessing
 num_cores = multiprocessing.cpu_count()
 Parallel(n_jobs=num_cores)(delayed(downloadPoster)(i) for i in df.itertuples(index=False))
