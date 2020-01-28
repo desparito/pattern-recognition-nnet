6ab11e62704321c5c714660ebefdef8cf74cb53b
Brian Janssen <tosti007@users.noreply.github.com>
2020-01-28 12:59:26 +0100

    Update README.mk

--- README.mk
+++ README.mk
@@ -4,32 +4,32 @@ To run the full algoritm from scratch a few steps are required. These steps are 
 ## Step 1: Setting up python
 A few dependencies are requirement to run the code. These can be installed in an virtual environment with:
 
-```bash
-python -m venv env
-source env/bin/activate
-pip install -r requirements.txt
+```
+$ python -m venv env
+$ source env/bin/activate
+$ pip install -r requirements.txt
 ```
 
 ## Step 2: Creating the dataset
 The origional dataset needs to be cleaned and all posters need to be downloaded. Firstly the data needs to be cleanedand then downloaded.
 
-```bash
-python data_clean.py
-python data_scrape.py
+```
+$ python data_clean.py
+$ python data_scrape.py
 ```
 
 ## Step 3: Creating YOLO data
 If you want to use YOLO classification to increase accuracy the yolo data needs to be created beforehand. This requires the weight dataset as well, which can be downloaded with the script below. With these weights a model will be created which in turn can be used to do the actual classification.
 
-```bash
-wget https://pjreddie.com/media/files/yolov3.weights
-python yolo_createmodel.py
-python yolo_classify.py
+```
+$ wget https://pjreddie.com/media/files/yolov3.weights
+$ python yolo_createmodel.py
+$ python yolo_classify.py
 ```
 
 ## Step 4: Running the neural network
 When all files are collected and created the network can be used.
 
-```bash
-python test.py
+```
+$ python test.py
 ```
